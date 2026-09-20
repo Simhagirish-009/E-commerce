b29bc045035d84c679aff6ff10edcac2c13b6982
simhagirishbotta24csm <bottasimhagirish.24.csm@anits.edu.in>
2026-09-20 23:54:43 +0530

    hi

--- src/apis/product_apis.js
+++ src/apis/product_apis.js
@@ -1,5 +1,5 @@
 import axiosInstance from "../apis/axiosInstance";
-const API_URL = "https://e-commerce-zhu2.onrender.com/";
+const API_URL = "https://e-commerce-zhu2.onrender.com";
 
 export const getCustomerData = async () => {
   return await axiosInstance.get(`${API_URL}/is-complete/`, {
