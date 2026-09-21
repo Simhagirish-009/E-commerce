f4c0e2dc77143ce80a5a8174cc45f57023af1511
simhagirishbotta24csm <bottasimhagirish.24.csm@anits.edu.in>
2026-09-21 07:34:25 +0530

    picss

--- myapp/models.py
+++ myapp/models.py
@@ -58,7 +58,7 @@ class Product(models.Model):
     discounted_price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     brand = models.CharField(max_length=255)
-    product_image = models.ImageField(upload_to='/')
+    product_image = models.ImageField(upload_to='product_images/')
 
     def __str__(self):
         return self.title
